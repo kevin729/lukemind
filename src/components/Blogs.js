1e043dba2fcb61882949b3b6c5852d8bfd4452eb
Kevin Martin <kevin@kevsbox.com>
2025-08-06 16:16:47 +0100

    Aftered CSS to remove bullet points only for header and sidebar lists and set display of to block after fetching CV and blog to their wrappers to prevent black padded square from appearing

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React from "react"
+import { useState, useEffect } from "react";
 import './styles/form.css'
 import './styles/blogs.css'
 import { Blogdata } from "./blogdata";
@@ -7,8 +8,24 @@ import { ForkLeft } from "@mui/icons-material"
 
 function Blogs() {
 
+    const URL = "https://www.googleapis.com/blogger/v3/blogs/2326199060955213897/posts/8676532119093612376?key=AIzaSyD_PqmobZ6RXv2W6LG_7FcpPUen9I0nM5Y"
+    const blogWrapperRef = React.createRef()
+    const [content, setContent] = useState()
+    useEffect(() => {
+        const fetchBlog = async (blogWrapper) => {
+            await fetch(URL)
+                .then(response => response.json())
+                .then(data => {
+                    blogWrapper.style.display = "block"
+                    setContent(data.content)
+                })
+        }
+
+        fetchBlog(blogWrapperRef.current)
+    })
+
     return (
-        <div className="blog-wrapper" dangerouslySetInnerHTML={{__html: BlogRetriever()}}>
+        <div className="blog-wrapper" ref={blogWrapperRef} dangerouslySetInnerHTML={{__html: BlogRetriever()}}>
         </div>
     )
 }
--- src/components/CV.js
+++ src/components/CV.js
@@ -4,22 +4,28 @@ import cv from './docs/cv.pdf'
 import "./styles/cv.css"
 
 const CV = () => { 
-     const URL = "https://www.googleapis.com/blogger/v3/blogs/2326199060955213897/posts/1069431430788802372?key=AIzaSyD_PqmobZ6RXv2W6LG_7FcpPUen9I0nM5Y"
-        const [content, setContent] = useState()
-        useEffect(() => {
-            const fetchCV = async () => {
-                await fetch(URL)
-                    .then(response => response.json())
-                    .then(data => setContent(data.content))
-            }
+    const URL = "https://www.googleapis.com/blogger/v3/blogs/2326199060955213897/posts/1069431430788802372?key=AIzaSyD_PqmobZ6RXv2W6LG_7FcpPUen9I0nM5Y"
+    const cvWrapperRef = React.createRef()
+    const [content, setContent] = useState()
     
-            fetchCV()
-        })
+     
+    useEffect(() => {
+        const fetchCV = async (cvWrapper) => {
+            await fetch(URL)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(cvWrapper)
+                    cvWrapperRef.current.style.display = 'block'
+                    setContent(data.content)
+                })
+        }
     
-        return (
-            <div id="cvWrapper" dangerouslySetInnerHTML={{__html: content}}></div>
-            
-        )
+        fetchCV(cvWrapperRef.current)
+    })
+
+    return (
+        <div id="cvWrapper" ref={cvWrapperRef} dangerouslySetInnerHTML={{__html: content}}></div>
+    )
 }
 
 export default CV;
