9664cf862d473b090dd39a09cda077932b75e674
Kevin Martin <kevin@kevsbox.com>
2025-08-16 02:12:00 +0100

    Updated Home screen

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -6,17 +6,18 @@ const Introduction = () => {
     return (
         <div id="introduction_wrapper">
             <div id="introduction">
-                <div id="about_me_wrapper">
-                    <h5 className="about_me">React JS</h5>
-                    <h5 className="about_me">Angular JS</h5>
-                    <h5 className="about_me">Android</h5>
-                </div>
-                <div id="me_wrapper">
-                    <Products />
-                </div>
-            </div>
-            <div id="title">
-                <h1>Junior Software Developer</h1>
+               <div id="about_me_wrapper">
+                    <div id="title_wrapper">
+                        <h1>Portsmouth Programmer</h1>
+                    </div> 
+                    <div id="skill_list_wrapper">
+                        <ul>
+                            <li>Web Dev</li>
+                            <li>Front End</li>
+                            <li>Android Dev</li>
+                        </ul>
+                    </div>
+               </div>
             </div>
         </div>
     )
---
+++ src/components/Updates.js
@@ -0,0 +1,11 @@
+const updates = () => {
+    return (
+        <div id="update_wrapper">
+            <div id="update">
+                <h2>Updates</h2>
+            </div>
+        </div>
+    )
+}
+
+export default updates
