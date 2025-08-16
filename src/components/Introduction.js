aabd893b3a6bcd67aae9ec15a4baad11e78007d5
Kevin Martin <kevin@kevsbox.com>
2025-08-16 06:15:48 +0100

    Added bullet points to intro

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -7,16 +7,19 @@ const Introduction = () => {
         <div id="introduction_wrapper">
             <div id="introduction">
                <div id="about_me_wrapper">
+                <div id="about_me">
                     <div id="title_wrapper">
                         <h1>Portsmouth Programmer</h1>
                     </div> 
                     <div id="skill_list_wrapper">
                         <ul>
-                            <li>Web Dev</li>
-                            <li>Front End</li>
-                            <li>Android Dev</li>
+                            <li>Full Stack Web Developer</li>
+                            <li>Database Management</li>
+                            <li>Mobile Responsive</li>
+                            <li>REST API User</li>
                         </ul>
                     </div>
+                </div>
                </div>
             </div>
         </div>
